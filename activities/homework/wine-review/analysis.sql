06279fe65539442633019e139f956bcbcfbed667
paulinamilenka <milenka280398@gmail.com>
2019-11-06 18:15:47 -0600

    new changes

--- activities/homework/wine-review/analysis.sql
+++ activities/homework/wine-review/analysis.sql
@@ -7,27 +7,102 @@
  */
 
 -- EX.1) Get the top 10 countries with more population density
+SELECT name FROM country WHERE area_km2=0;
 
+
+SELECT
+    name,
+    population /area_km2 AS density
+FROM
+    country
+WHERE
+    area_km2 > 0
+ORDER BY "density" DESC
+LIMIT 10
+;
 
 -- EX.2) Get the count of male/female tasters.
+SELECT DISTINCT gender FROM taster;
+SELECT
+    gender,
+       count(*)
+FROM
+    taster
+WHERE
+    --LOWER (gender) IN ('male', 'female')
+    --LOWER(gender) NOT LIKE 'undef%'
+    LOWER(gender) LIKE '%male'
+GROUP BY
+    gender
 
+;
 
 -- EX.3) Get the percentage of male/female tasters.
+WITH taster_valid AS (
+    SELECT *
+    FROM taster
+    WHERE LOWER(gender) IN ('male', 'female')
+    ), taster_gender_agg AS (
+    SELECT
+        gender,
+        COUNT(*)::NUMERIC(7,2) gender_sum
+    FROM
+        taster_valid
+    GROUP BY gender
+    ), taster_total as (
+        SELECT
+            count(*)::NUMERIC(7,2) total
 
-
+        FROM
+            taster_valid
+)
+SELECT
+    gender,
+    TRUNC(100*gender_sum/total,2) percentage
+FROM
+    taster_gender_agg,
+     taster_total
+;
 -- EX.4) How many countries share the same first digit on their country-code?
 -- Show only those digits with more than 20 countries.
+SELECT DISTINCT LEFT(code,1) FROM country;
 
+SELECT
+    LEFT(code,1) first_digit,
+    count(*) country_count
+FROM
+     country
+
+GROUP BY
+    first_digit
+HAVING
+    count(*) > 20
+;
 
 -- EX.5) Get the % of countries are not labeled as a trillion usd gdp and
 -- do have a null happiness_score.
+SELECT count(*) FROM country;
+SELECT
+    100*count(*)/ MAX (t.total) Percentage
+
+FROM
+     country,
+     (SELECT count(*) total FROM country) t
+
+WHERE
+    --LOWER (split_part(gdp_usd, ' ', 2)) != 'trillion'
+    LOWER(gdp_usd) NOT LIKE '%trillion'
+    AND happiness_score IS NULL;
 
 
 -- COUNTRY ANALYSIS
 
 -- A) Get the average happiness_score of the countries labeled with a GDP
 -- of "billion" and "trillion".
-
+SELECT
+    AVG(happiness_score)
+    FROM country
+    WHERE gdp_usd= 'Billion' AND gdp_usd = 'Trillion'
 
 -- B) Show a table with the country name, population, area, gdp, and happiness core of the
 -- the G7 countries (i.e., `Canada`, `France`, `Germany`, `Italy`, `Japan`, `United Kingdom`, `United States`)
