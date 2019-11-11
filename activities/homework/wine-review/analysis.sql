e61ef4d60e1b98e7e0757826d0e3c83dd9308575
paulinamilenka <milenka280398@gmail.com>
2019-11-11 14:34:03 -0600

    Homework sql

--- activities/homework/wine-review/analysis.sql
+++ activities/homework/wine-review/analysis.sql
@@ -99,44 +99,140 @@ WHERE
 
 -- A) Get the average happiness_score of the countries labeled with a GDP
 -- of "billion" and "trillion".
+SELECT gdp_usd from country;
+select happiness_score from country;
+
+
 SELECT
     AVG(happiness_score)
-    FROM country
-    WHERE gdp_usd= 'Billion' AND gdp_usd = 'Trillion'
+FROM
+    country
+WHERE
+    LOWER(gdp_usd) NOT LIKE 'million'
+    AND gdp_usd IS NOT NULL
+;
 
 -- B) Show a table with the country name, population, area, gdp, and happiness core of the
 -- the G7 countries (i.e., `Canada`, `France`, `Germany`, `Italy`, `Japan`, `United Kingdom`, `United States`)
 -- order by happiness_score (DESC).
-
+SELECT name from country;
+SELECT
+    name, population, area_km2, gdp_usd, happiness_score
+FROM
+    country
+WHERE
+    lower(name) like 'canada'
+    or lower(name) like 'france'
+    or lower(name) like 'germany'
+    or lower(name) like 'italy'
+    or lower(name) like 'japan'
+    or lower(name) like 'united kingdom'
+    or lower(name) like 'united states'
+;
 
 -- C) Create a custom score called "score" using this formula: happiness_score * density
 -- Where `density` is population / area_km2. Show the top 10 countries (name) with greater score.
-
+select happiness_score FROM country
+select area_km2 from country
+SELECT
+       name,
+       trunc(numeric_mul(happiness_score,DIV(population,area_km2)),2)  AS "score"
+FROM
+    country
+WHERE area_km2 > 0 AND happiness_score IS NOT NULL
+ORDER BY "score"  DESC
+    LIMIT 10;
 
 -- WINE ANALYSIS
 
 -- D) Get the number of wines per variety, ordered by the latter (desc)
-
+SELECT DISTINCT variety from wine;
+SELECT
+    w.variety AS "Variety",
+    count(*) AS "Total variety"
+FROM
+    country c INNER JOIN wine w ON c.id = w.country_id
+GROUP BY
+    w.variety
+ORDER BY
+    "Total variety" DESC
+;
 
 -- E) How many wines are registered per country? Show country name and number of wines, ordered by the latter (desc).
 
-
+SELECT
+    c.name AS "Country",
+    count(*) AS "Total wine variety"
+FROM
+    country c INNER JOIN wine w ON c.id = w.country_id
+GROUP BY
+    c.name
+ORDER BY
+    "Total wine variety" DESC
+;
 -- REVIEW ANALYSIS
 
 -- F) What's the average wine price and points per province?
 -- Show the province, avg-price, avg-points when the avg-points are grater than 85.
 -- Ordered by avg-price and then by avg-points.
+SELECT DISTINCT province from wine;
+SELECT
+    w.province AS "Province",
+    AVG(r.price)::numeric(7,2) AS "Average wine price",
+    AVG(r.points)::numeric(7,2) AS "Average wine points"
+FROM
+    country c INNER JOIN wine w ON c.id = w.country_id INNER JOIN review r ON w.id = r.wine_id
+GROUP BY
+    w.province
+ORDER BY
+    "Average wine price",
+    "Average wine points"
+;
 
 
 -- G) What's the average wine price and points of the countries with more than a 7 in their happiness score?
 -- Show the country, avg-price, avg-points.
 -- Ordered by avg-points and then by avg-price.
-
+SELECT DISTINCT happiness_score, name from country ;
+SELECT
+    c.name AS "Country",
+    AVG(r.price)::numeric(7,2) AS "Average wine price",
+    AVG(r.points)::numeric(7,2) AS "Average wine points"
+FROM
+    country c INNER JOIN wine w ON c.id = w.country_id INNER JOIN review r ON w.id = r.wine_id
+WHERE
+    c.happiness_score > 7
+GROUP BY
+    c.happiness_score,
+    c.name
+ORDER BY
+    "Average wine points",
+    "Average wine price"
+;
 
 -- H) What's the min, avg, and max wine points per taster gender (excluding undefined) and wine variety starting with "Cabernet".
 -- Order by: variety, gender
-
+SELECT
+    t.gender,
+    max(r.points),
+    min(r.points),
+    avg(r.points)
+FROM
+    review r INNER JOIN taster t ON r.taster_id = t.id INNER JOIN wine w ON r.wine_id = w.id
+WHERE
+    LOWER(t.gender) NOT LIKE 'undef%'
+    AND LOWER(w.variety) LIKE 'cabernet'
+GROUP BY
+    t.gender;
 
 -- I) Create the following custom score called "wine_quality_and_happiness_index": happiness_score * avg(points) / 100
 -- Get the score per country and order by the value (desc).
-
+SELECT
+    c.name,
+       trunc(c.happiness_score*avg(r.points)/100,2) AS  "wine_quality_and_happiness_index"
+FROM
+    country c INNER JOIN wine w ON c.id = w.country_id INNER JOIN review r ON w.id = r.wine_id
+GROUP BY
+    r.points, c.happiness_score, c.name
+ORDER BY
+    "wine_quality_and_happiness_index" DESC;
